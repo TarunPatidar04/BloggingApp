4c546e7a446f751a0f81ae6290904385a8ff92c7
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-04-30 00:37:23 +0530

    upload Image and create a Blog

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const UserRouter = require("./routes/user");
+const Blog = require("./models/blog");
+const blogRouter = require("./routes/blog");
 const { checkForAuthenticationCookie } = require("./middleware/authentication");
 require("dotenv").config();
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.resolve("./public")));
 app.use(checkForAuthenticationCookie("token"));
 mongoose
   .connect("mongodb://localhost:27017/blogify")
@@ -21,13 +24,16 @@ mongoose
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const allBlog = await Blog.find({});
   res.render("home", {
     user: req.user,
+    blogs: allBlog,
   });
 });
 
 app.use("/user", UserRouter);
+app.use("/blog", blogRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is started at ${process.env.PORT}`);
